efb03d1a7dd268eddbf3a977ae47c2f03442373e
Eulersghost <learner_studios@proton.me>
2026-07-11 20:56:42 -0700

    Auto-commit

---
+++ Lesson_1_2.ps1
@@ -0,0 +1,34 @@
+#Asignment answers
+
+Update-Help
+
+Out-File
+#there are 9 cmdlets
+Set-PSBreakpoint
+
+#create
+New-Alias
+#modify
+Set-Alias
+#export
+Export-Alias
+#import
+Import-Alias
+
+#for keyboard history use the alias 'h' or  use 'history', Get-Histroy and then pipe it to Out-File 
+Get-History | Out-File keyboard2.txt
+
+Get-PSHostProcessInfo | ProcessName *
+
+Get-Process -Name #got by a specific list of process name   
+
+Get-Process -Name -IncludeUserName #some name of the PRocessNAme#
+
+Invoke-Command #runs on local and remote session
+
+$PSDefaultParameterValues['Out-File:Width'] = 2000 # this has to be run before invoking Out-File normally the -Width 
+
+-NoClobber # when using Out-File this prevents a current named file from being overwritten
+
+Get-Alias #get's the list of aliases
+
