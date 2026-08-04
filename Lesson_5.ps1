222a6bcc71e545ef3423b8044652e3789ba5bccb
Jason Learner <learner_studios@proton.me>
2026-08-04 16:30:16 -0700

    Auto-commit

---
+++ Lesson_5.ps1
@@ -0,0 +1,7 @@
+#Create a new director called Labs
+New-Item -Name "Labs" -ItemType Directory
+
+#create a zero-length file named /Labs/Test.txt
+New-Item -Name "./test.txt" -Path C:\Users\jlearner\Labs\
+
+
